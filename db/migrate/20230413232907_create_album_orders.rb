fd2ab4953926e1de6efc52cecc197e1767660f8a
Amanda Trigueros <amandatrigueros01@gmail.com>
2023-04-13 17:29:49 -0600

    add: albums_orders model

---
+++ db/migrate/20230413232907_create_album_orders.rb
@@ -0,0 +1,11 @@
+class CreateAlbumOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :album_orders do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :album, null: false, foreign_key: true
+      t.integer :album_copies
+
+      t.timestamps
+    end
+  end
+end
