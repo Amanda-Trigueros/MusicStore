582e450e93200f4524e5f56b6f5ce1df7cd8d76a
Alex Mendoza <alex.mendoza.et@gmail.com>
2023-04-14 12:59:33 -0500

    added artist validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,17 @@
 class Artist < ApplicationRecord
-end
+  validates :name, presence: true
+  validates :birth_date, presence: true, unless: -> { death_date.blank? }
+
+  validates :birth_date, comparison: { less_than: :death_date }, allow_blank: true
+
+  validate :birth_date_cant_be_in_the_future
+  
+  def birth_date_cant_be_in_the_future
+    return unless birth_date.present? && birth_date > Time.zone.today
+
+    errors.add(:birth_date, "can't be in the future")
+  end
+
+
+end 
+
